b8a223124aba7d6dcb3617adc0920ae35ca62cf8
David Maisonave <47364845+David-Maisonave@users.noreply.github.com>
2023-06-11 14:12:54 -0400

    Update SQLiteExtensions.cs

--- SQLiteExtensions.cs
+++ SQLiteExtensions.cs
@@ -1,5 +1,5 @@
 ﻿/* ***************************************************************************
- * Copyright © 2022 David Maisonave	(https://axter.com)	All rights reserved.
+ * Copyright © 2023 David Maisonave	(https://axter.com)	All rights reserved.
  * ******************************************************************************************************************************************************
  * The is free software. You can redistribute it and/or modify it under the terms of the MIT License. For more information, please see License file 
  * distributed with this package.
@@ -60,6 +60,8 @@ namespace SQLiteExtensions
 
     using System.Diagnostics;
 
+    // using Windows.Storage;
+
     /// Extension methods for SQLite. (SQLiteConnection and SQLiteCommand)
     ///Copy table data
     ///  Options:
@@ -78,46 +80,201 @@ namespace SQLiteExtensions
     /// </usage>
     public static class SqliteExt
     {
+        ///////////////////////////////////////////////////////////////////////////
         // API's using common SQL terms
         static public int Insert(this SQLiteConnection conn, string InsertQueryCmd) => Execute(conn, InsertQueryCmd);
-        static public int Insert(this SQLiteCommand sqliteCommand, string InsertQueryCmd) => Execute(sqliteCommand, InsertQueryCmd);
-        static public int DropTable(this SQLiteCommand sqliteCommand, string TableName)=>sqliteCommand.Execute($"DROP TABLE if EXISTS '{TableName}';");
-        static public int DeleteFrom(this SQLiteCommand sqliteCommand, string TableName)=> sqliteCommand.Execute($"DELETE from {TableName}");
+        static public int Insert(this SQLiteCommand sql_cmd, string InsertQueryCmd) => sql_cmd.Execute(InsertQueryCmd);
+        static public int DropTable(this SQLiteCommand sql_cmd, string TableName) => sql_cmd.Execute($"DROP TABLE if EXISTS '{TableName}';");
+        static public int DeleteFrom(this SQLiteCommand sql_cmd, string TableName) => sql_cmd.Execute($"DELETE from {TableName}");
+        static public SQLiteDataReader Query(this SQLiteCommand sql_cmd, string selectQuery) => sql_cmd.CreateReader(selectQuery);
 
+        ///////////////////////////////////////////////////////////////////////////
         // API's to create SQLiteConnection, SQLiteCommand, and SQLiteDataReader
-        static public SQLiteConnection CreateConnection(string connectionString)  // This is the only API which requires SqliteExt to be used.  Example:  SqliteExt.CreateConnection(@"Data Source=C:\mydatabase.db;Mode=ReadOnly")
+
+        /// <summary>
+        ///     Creates SQLiteConnection using input connection string
+        /// </summary>
+        /// <param name="dbFilePath">File path to DB file to open</param>
+        /// <param name="CreateTablesCmd">If this value is NOT null and DB file does NOT exists, DB file is created using create table command</param>
+        /// <param name="AutoOpenConnection">If true, opens the connection</param>
+        /// <param name="CkStartsWithDataSource">If true, checks if connections string starts with "Data Source", and if not, inserts it.</param>
+        /// <param name="FastConnection">If true, passes pragma value to increase SQLite connection performance.</param>
+        /// <param name="DoRethrow">If true, re-throws any exception to let calling function capture it.</param>
+        ///  <returns>
+        ///      A SQLiteConnection
+        ///      Returns null on failure
+        ///  </returns>      
+        /// <usage>
+        ///     Example #1:
+        ///         using ( SQLiteConnection? conn = SqliteExt.CreateConnection(".\\myDbFile.db") )
+        ///         {
+        ///             SQLiteCommand sql_cmd = conn.CreateCommand();
+        ///             foreach ( FileInfo file in fileInfos )
+        ///                 sql_cmd.Execute($"INSERT OR REPLACE  into FileProperties (ParentDir, Name, Ext, Size) values (\"{file.Name}\", \"{file.DirectoryName}\", \"{file.Extension}\", {file.Length})");
+        ///         }
+        ///     Example #2:
+        ///         string createTableCmd = "CREATE TABLE \"FileProperties\" (\r\n\t\"ParentDir\"\tTEXT NOT NULL,\r\n\t\"Name\"\tTEXT NOT NULL,\r\n\t\"Ext\"\tTEXT NOT NULL,\r\n\t\"MediaType\"\tINTEGER NOT NULL DEFAULT 0,\r\n\t\"Size\"\tINTEGER NOT NULL DEFAULT 0,\r\n\t\"Duration\"\tINTEGER NOT NULL DEFAULT 0,\r\n\t\"CheckSum\"\tINTEGER NOT NULL DEFAULT 0,\r\n\t\"FingerPrint\"\tBLOB,\r\n\tPRIMARY KEY(\"Name\",\"ParentDir\")\r\n);";
+        ///         using ( SQLiteConnection? conn = SqliteExt.CreateConnection(".\\Database\\myDbFile.db", createTableCmd) )
+        /// </usage>
+        static public SQLiteConnection CreateConnection(string dbFilePath, string CreateTablesCmd = null, bool AutoOpenConnection = true, bool CkStartsWithDataSource = true, bool FastConnection = true, bool DoRethrow = false)
         {
-            var conn = new SQLiteConnection(connectionString);
+            string UsingConnectionString = ( CkStartsWithDataSource && dbFilePath.ToLower().StartsWith("data source") == false ) ? "Data Source=" + dbFilePath: dbFilePath;
+            try
+            {
+                bool CreateTables = false;
+                if ( CreateTablesCmd != null && dbFilePath.ToLower().StartsWith("data source") == false && !File.Exists(dbFilePath) )
+                {
+                    SQLiteConnection.CreateFile(dbFilePath);
+                    CreateTables = true;
+                }
+                SQLiteConnection conn = new (UsingConnectionString);
+                if ( AutoOpenConnection && conn != null )
+        {
             conn.Open();
+                    if ( FastConnection )
+                        conn.Execute(@"PRAGMA journal_mode = 'wal'", true);
+                    if ( CreateTables )
+                        conn.Execute(CreateTablesCmd, true);
+                }
             return conn;
         }
-        static public SQLiteCommand CreateCommand(this SQLiteConnection conn, string cmd = "") => conn.CreateCommand(cmd);
-        static public SQLiteDataReader CreateReader(this SQLiteConnection conn, string cmd)=> conn.CreateCommand(cmd).ExecuteReader();
-        static public SQLiteDataReader CreateReader(this SQLiteCommand sqliteCommand, string cmd)
+            catch(Exception ex)
         {
-            sqliteCommand.CommandText = cmd;
-            return sqliteCommand.ExecuteReader();
+                Debug.WriteLine($"Error: SQL CreateConnection failure using connection string '{UsingConnectionString}'; Input param:'{dbFilePath}', {CreateTablesCmd}, {AutoOpenConnection}, {CkStartsWithDataSource}, {FastConnection}\nErrMsg: {ex.Message}");
+                if ( DoRethrow )
+                    throw ex;
         }
+            return null;
+        }
+
+        /// <summary>
+        ///     Overrides the default behavior of DbDataReader to return a specialized SQLiteDataReader class
+        /// </summary>
+        /// <param name="sql_cmd">SQL DB SQLiteCommand</param>
+        /// <param name="selectQuery">Select query command</param>
+        /// <param name="DoRethrow">If true, re-throws any exception to let calling function capture it.</param>
+        ///  <returns>
+        ///      A SQLiteDataReader
+        ///      Returns null on failure
+        ///  </returns>
+        /// <usage>
+        ///     Example #1:
+        ///         SQLiteCommand sql_cmd = conn.CreateCommand();
+        ///         SQLiteDataReader queryResults = sql_cmd.CreateReader("select * from fooTable");
+        ///     Example #2:
+        ///         See GetTables && GetTableList implementation
+        /// </usage>
+        static public SQLiteDataReader CreateReader(this SQLiteCommand sql_cmd, string selectQuery, bool DoRethrow = false)
+        {
+            try
+            {
+                sql_cmd.CommandText = selectQuery;
+                return sql_cmd.ExecuteReader();
+            }
+            catch ( Exception ex )
+            {
+                Debug.WriteLine($"Error: SQL CreateReader failure for query '{selectQuery}'\nErrMsg: {ex.Message}");
+                if ( DoRethrow )
+                    throw ex;
+            }
+            return null;
+        }
+
         /// <summary>
         /// Execute Non-Query. Example: Insert, Insert or Replace, Drop, Create, etc...
         /// </summary>
         /// <param name="conn">SQL DB connection</param>
         /// <param name="NonQueryCmd">SQL Non-Query (Insert, Insert or Replace, Drop, Create)</param>
-        static public int Execute(this SQLiteConnection conn, string NonQueryCmd)
+        /// <param name="DoRethrow">If true, re-throws any exception to let calling function capture it.</param>
+        ///  <returns>
+        ///      The number of rows inserted/updated affected by it.
+        ///      Returns negative number on failure
+        ///  </returns>
+        /// <usage>
+        ///     Example #1:
+        ///         SQLiteConnection? conn = SqliteExt.CreateConnection(".\\myDbFile.db");
+        ///         conn.Execute($"DELETE from {TableName}");
+        ///     Example #2:
+        ///         SQLiteConnection? conn = SqliteExt.CreateConnection(".\\myDbFile.sqlite");
+        ///         conn.Execute($"INSERT OR REPLACE into foofoo (Name, Size) values (\"{foo_Name}\", {foo_Size})");
+        /// </usage>
+        static public int Execute(this SQLiteConnection conn, string NonQueryCmd, bool DoRethrow = false)
         {
-            SQLiteCommand sqliteCommand = conn.CreateCommand(NonQueryCmd);
-            return sqliteCommand.ExecuteNonQuery();
+            try
+            {
+                SQLiteCommand sql_cmd = conn.CreateCommand();
+                sql_cmd.CommandText = NonQueryCmd;
+                return sql_cmd.ExecuteNonQuery();
+            }
+            catch ( Exception ex )
+            {
+                Debug.WriteLine($"Error: SQL Execute failure for query '{NonQueryCmd}'\nErrMsg: {ex.Message}");
+                if ( DoRethrow )
+                    throw ex;
+            }
+            return -1;
         }
         /// <summary>
         /// Execute Non-Query. Example: Insert, Insert or Replace, Drop, Create, etc... 
         /// </summary>
-        /// <param name="sqliteCommand">SQL DB connection</param>
+        /// <param name="sql_cmd">SQL DB connection</param>
         /// <param name="NonQueryCmd">SQL Non-Query (Insert, Insert or Replace, Drop, Create)</param>
-        static public int Execute(this SQLiteCommand sqliteCommand, string NonQueryCmd)
+        /// <param name="DoRethrow">If true, re-throws any exception to let calling function capture it.</param>
+        ///  <returns>
+        ///      The number of rows inserted/updated affected by it.
+        ///      Returns negative number on failure
+        ///  </returns>
+        /// <usage>
+        ///     Example #1:
+        ///         SQLiteCommand sql_cmd = conn.CreateCommand();
+        ///         sql_cmd.Execute($"DELETE from {TableName}");
+        ///     Example #2:
+        ///         sql_cmd.Execute($"INSERT into foofoo (Name, Size) values (\"{foo_Name}\", {foo_Size})"");
+        /// </usage>
+        static public int Execute(this SQLiteCommand sql_cmd, string NonQueryCmd, bool DoRethrow = false)
         {
-            sqliteCommand.CommandText = NonQueryCmd;
-            return sqliteCommand.ExecuteNonQuery();
+            try
+            {
+                sql_cmd.CommandText = NonQueryCmd;
+                return sql_cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+        {
+                Debug.WriteLine($"Error: SQL Execute failure for query '{sql_cmd.CommandText}'\nErrMsg: {ex.Message}");
+                if ( DoRethrow )
+                    throw ex;
         }
+            return -1;
+        }
+
+        ///////////////////////////////////////////////////////////////////////////
+        // Table API's
+
+        /// <summary>
+        ///     Executes query for tales in DB schema
+        /// </summary>
+        ///  <returns>
+        ///     Returns query results with all the tables in the DB schema
+        ///  </returns>
+        static public SQLiteDataReader GetTables(this SQLiteCommand sql_cmd) => sql_cmd.CreateReader("select name from sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';");
+        
+        /// <summary>
+        ///     Executes query for tales in DB schema
+        /// </summary>
+        ///  <returns>
+        ///     Returns a list of all the tables in the DB schema
+        ///  </returns>
+        static public List<string> GetTableList(this SQLiteCommand sql_cmd)
+        {
+            List<string> tableList = new();
+            SQLiteDataReader reader = sql_cmd.GetTables();
+            while ( reader.Read() )
+            {
+                tableList.Add(reader.GetString(0));
+            }
+            return tableList;
+        }
+
         /// <summary>
         /// Create table and insert table data from one DB (connection) to another.
         /// The table name is the same in source and destination DB (connection).
@@ -183,8 +340,23 @@ namespace SQLiteExtensions
         /// <param name="SqlInsertCmd">Used this to overwrite insert command. Can be one of the following: "INSERT", "INSERT OR REPLACE" </param>
         /// <param name="deleteDestinationTableFirst">If true, deletes destination table content before executing insert.</param>
         static public bool CopyTable(this SQLiteConnection sourceConnection, string sourceTableName, string destinationTableName = null, SQLiteConnection destinationConnection = null, string SqlInsertCmd = "INSERT", bool deleteDestinationTableFirst = false, bool createTableFirst = false, bool TransactionMode = true) => CopyTableImpl(sourceConnection, sourceTableName, destinationTableName, destinationConnection, SqlInsertCmd, deleteDestinationTableFirst, createTableFirst, TransactionMode);
-
-#region SqliteExt Private methods
+        static public string defaultAppDbFile
+        {
+            get
+            {
+                String AppPath = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+                String ProductName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+                if (!Directory.Exists(AppPath))
+                    Directory.CreateDirectory(AppPath);
+                if ( !Directory.Exists($"{AppPath}\\{DbPath}") )
+                    Directory.CreateDirectory($"{AppPath}\\{DbPath}");
+                Debug.Assert(Directory.Exists($"{AppPath}\\{DbPath}"));
+                return $"{AppPath}\\{DbPath}\\{ProductName}.db";
+            }
+        }
+        static public string DbPath { get; } = "Database";
+        
+        #region SqliteExt Private methods
         static private bool CopyTableImpl(this SQLiteConnection sourceConnection, string sourceTableName, string destinationTableName = null, SQLiteConnection destinationConnection = null, string SqlInsertCmd = "INSERT", bool deleteDestinationTableFirst = false, bool createTableFirst = false, bool TransactionMode = true)
         {
             destinationTableName ??= sourceTableName;
